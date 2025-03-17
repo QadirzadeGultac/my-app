60c54c29f30160cc4717ddd442d727d97496e30d
QadirzadeGultac <gultac.qadirzade@mail.ru>
2025-03-17 15:59:34 +0400

    refreshh

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { HashRouter } from 'react-router-dom'
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <HashRouter>
     <App />
   </HashRouter>
-)
+);
+
